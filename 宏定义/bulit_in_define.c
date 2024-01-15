f3333a433a2d208517bc074ee9eb972964ce16eb
PinkStarV <130140140+PinkStarV@users.noreply.github.com>
2024-01-15 15:46:19 +0800

    Create bulit_in_define.c

---
+++ 宏定义/bulit_in_define.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("__DATE__ = %s\n",__DATE__);
+    printf("__TIME__ = %s\n",__TIME__);
+    printf("__FILE__ = %s\n",__FILE__);
+    printf("__LINE__ = %d\n",__LINE__);
+    printf("__func__ = %s\n",__func__);
+    printf("__PRETTY_FUNCTION__ = %s\n",__PRETTY_FUNCTION__);
+    return 0;
+}
