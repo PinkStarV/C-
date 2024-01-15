7a4c3ffc26087c9e6487b9b0202fd5cefc3f9c4a
PinkStarV <130140140+PinkStarV@users.noreply.github.com>
2024-01-15 16:01:26 +0800

    Update 6.terminal_output.c

--- 重学输入输出/6.terminal_output.c
+++ 重学输入输出/6.terminal_output.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    freopen("/dev/pts/1","w",stdout);
+    freopen("******","w",stdout);
     char s[1000];
     while(scanf("%s",s) != EOF){
         printf("from tty0 : %s\n",s);
